4bc90fb323b408a093702955c2f45d5d87e37071
NatBat92 <natalie.neale1992@outlook.com>
2023-04-12 12:11:11 +0000

    add allauth module

--- guild_wars_2/urls.py
+++ guild_wars_2/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('guild_wars_2_blog.urls'), name='gw2_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
